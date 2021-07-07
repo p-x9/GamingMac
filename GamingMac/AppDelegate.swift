7cf0493a9d7a50a7d9394a338a92496f1014f9e5
p-x9 <50244599+p-x9@users.noreply.github.com>
2021-07-07 19:06:03 +0900

    Initial Commit

---
+++ GamingMac/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  GamingMac
+//
+//  Created by p-x9 on 2021/07/07.
+//  
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GamingMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GamingMac
+//
+//  Created by p-x9 on 2021/07/07.
+//  
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
